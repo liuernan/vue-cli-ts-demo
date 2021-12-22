e58d2e83cf8003104621e0c9b441539b1857c002
liuernan <liujiaweidreamer@gmail.com>
2021-12-22 10:53:05 +0800

    Initial data can be declared as instance properties

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="about">
-    <h1>hello</h1>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
 
-export default class About extends Vue {}
+export default class About extends Vue {
+  message = "Initial data can be declared as instance properties";
+}
 </script>
